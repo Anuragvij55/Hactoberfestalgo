e3989f2daecd007666b0e455e03c8c83fceff808
Ashish <ash242242@gmail.com>
2022-10-14 00:33:14 +0530

    MaximumSubarray.java

---
+++ Maximums\Subarray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxSubArray(int[] arr) {
+//         kadanes Algorithm for largest subarray
+      int mx=Integer.MIN_VALUE;
+        int n=arr.length;
+        int sum=0;
+        for(int i=0;i<n;i++){
+        sum+=arr[i];
+          
+            mx=Math.max(mx,sum);
+              if(sum<0) sum=0;
+            
+            
+        }
+        return mx;
+    
+    }
+}
